f68fe43a392e04c02aefadab1545952cdbf02643
jhennymatias <jhennifercristinematias@gmail.com>
2022-10-03 23:09:28 -0300

    rascunho do show

--- sistemainv/src/Pages/ItemList/index.js
+++ sistemainv/src/Pages/ItemList/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Nav from "../../Components/NavBar"
-import { MdAddCircle, MdOutlineRemoveRedEye, MdDeleteOutline, MdMode } from 'react-icons/md'
+import { MdAddCircle, MdOutlineRemoveRedEye, MdDeleteOutline, MdMode, MdDone, MdWarning, MdOutlineQrCode, MdQrCode } from 'react-icons/md'
 import './styles.css'
 import swal from 'sweetalert';
-import Modal from '@mui/material/modal'
+import Modal from '@mui/material/modal';
+import Logo from '../../assets/logo.png';
 export default function ItemList() {
     const [close, setClose] = React.useState(false);
     const [open, setOpen] = React.useState(false);
@@ -31,15 +32,30 @@ export default function ItemList() {
     return (
 
         <div className="item-list-container">
-        
-<Modal
-  open={open}
-  onClose={handleCloseModal}
-  aria-labelledby="modal-modal-title"
-  aria-describedby="modal-modal-description"
->
-  <div className="modal" >modal</div>
-</Modal>
+
+            <Modal
+                open={open}
+                onClose={handleCloseModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <div className="modal" >
+                    <h1>MOUSE</h1>
+                    <p>DESCRIÇÃO: Lorem ipsum dolor sit amet, 
+                        consectetur adipiscing elit. Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit. Sed placerat ut odio ac congue. 
+                        Cras. Sed placerat ut odio ac congue. Cras.
+                    </p>
+                    <div className="inferior-modal">
+                        <div className="info">
+                            <p>ESTADO: <MdDone /></p>
+                            <p>LOCAL: sala 110 Bloco C - UFSC Araranguá</p>
+                            <MdQrCode size={70}/>
+                        </div>
+                        <img src={Logo} />
+                    </div>
+                </div>
+            </Modal>
             <Nav />
             <div className="item-list-content">
                 <div className="item-list-header">
