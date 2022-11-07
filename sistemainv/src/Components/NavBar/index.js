f48d5c672619e5daa9c74fc8c30f04d238f503e9
jhennymatias <jhennifercristinematias@gmail.com>
2022-11-06 22:18:15 -0300

    verificar se esta logado

--- sistemainv/src/Components/NavBar/index.js
+++ sistemainv/src/Components/NavBar/index.js
@@ -3,12 +3,18 @@ import './styles.css'
 import {MdPerson} from 'react-icons/md'
 
 export default function Nav(){
+    const info = localStorage.getItem("usuarioLogado")
+    const data = JSON.parse(info);
+
+    if(data.nome == '' || data.matricula == '' || data.email == ''){
+        window.location.href = '/login';
+    }
     return(
         <nav>
             <a href="/"><img src={Logo} width="300px"/></a>
             <div className='person'>
                 <MdPerson color='white'/>
-                <p>Jhennifer Matias</p>
+                <p>{data.nome}</p>
             </div>
             
         </nav>
